171e4cb5cc122741e5fb8cc67438e983c1d869a8
Shrey Kshatriya <shreykshatriya@gmail.com>
2022-11-08 00:19:26 -0500

    Update strings.py

--- Day 4/strings.py
+++ Day 4/strings.py
@@ -71,6 +71,62 @@ b = val.split()
 print(b[0][0] + b[1][0] + b[2][2])
 
 ## 20. Use index to determine the position of the first occurrence of C in Coding For All.
-print(val.find('C'))
+print(val.index('C'))
 
-## 21. 
+## 21. Use index to determine the position of the first occurrence of F in Coding For All.
+print(val.index('F'))
+
+## 22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
+print(val.rfind('l'))
+
+## 23. Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+sent = 'You cannot end a sentence with because because because is a conjunction'
+print(sent.find('because'))
+
+## 24. Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+print(sent.rfind('because'))
+
+## 25.  Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+print(sent.replace('because',''))
+
+## 26. Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+print(sent.find('because'))
+
+## 27. Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+print(sent.replace('because',''))
+
+## 28. Does ''Coding For All' start with a substring Coding?
+print(val.startswith('Coding'))
+
+## 29. Does 'Coding For All' end with a substring coding?
+print(val.endswith('coding'))
+
+## 30. '   Coding For All      '  , remove the left and right trailing spaces in the given string.
+print('   Coding For All      '.strip(" "))
+
+## 31. Which one of the following variables return True when we use the method isidentifier():
+## 30DaysOfPython
+## thirty_days_of_python
+a,b = "30DaysOfPython","thirty_days_of_python"
+print(a.isidentifier)
+print(b.isidentifier())
+
+## 32. The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+l = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('# '.join(l))
+
+## 33. Use the new line escape sequence to separate the following sentences. 
+print("I am enjoying this challenge.\nI just wonder what is next.")
+
+## 34. Use a tab escape sequence to write the following lines.
+print("Name\tAge\tCountry\tCity\nShrey\t200\tUSA\tNYC")
+
+## 35. Use the string formatting method to display the following:
+radius = 10
+area = 3.14 * radius ** 2
+print(f"The area of a circle with radius {radius} is {area} meters square.")
+
+## 36. Make the following using string formatting methods:
+print('8 + 6 = 14')
+print('8 - 6 = 2')
+print('8 * 6 = 48\n8 / 6 = 1.33\n8 % 6 = 2\n8 // 6 = 1\n8 ** 6 = 262144')
