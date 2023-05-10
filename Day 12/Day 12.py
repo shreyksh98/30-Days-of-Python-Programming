b58f1e045a696234ed6a31ac6366cee4253001e3
Shrey Kshatriya <shreykshatriya@gmail.com>
2023-05-09 21:25:40 -0400

    added all the questions and answers

--- Day 12/Day 12.py
+++ Day 12/Day 12.py
@@ -25,4 +25,31 @@ print(rgb_color_gen())
 
 # Exercise 2
 
-# 2.1
+# 2.1 Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array (six hexadecimal numbers written after #. Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples).
+
+from modulo import gen_hex
+print(gen_hex())
+
+# 2.2 Write a function list_of_rgb_colors which returns any number of RGB colors in an array.
+
+from modulo import list_of_rgb_colors
+print(list_of_rgb_colors(2))
+
+# 2.3 Write a function generate_colors which can generate any number of hexa or rgb colors.
+
+from modulo import generate_colors
+
+print(generate_colors('rgb',2))
+
+# Exercise 3
+
+# 3.1 Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+
+from modulo import shuffle_list
+print(shuffle_list([1,2,3,4,5,6,7]))
+
+# 3.2 Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
+
+from modulo import rand_int
+print(rand_int())
+
--- Day 12/modulo.py
+++ Day 12/modulo.py
@@ -17,4 +17,59 @@ def rgb_color_gen():
     b = randint(0,255)
     return(r,g,b)
 
-# def gen_hex():
+def gen_hex():
+    len = 6
+    a = 'abcdef'
+    hex = str(''.join(random.choices(string.digits+a, k = len)))
+    hex = '#'+hex
+    return hex
+
+def list_of_rgb_colors(args):
+    lst = []
+    for i in range(0,args):
+        r = randint(0,255)
+        g = randint(0,255)
+        b = randint(0,255)
+        rgb = r,g,b
+        rgb = 'rgb'+str(rgb)
+        lst.append(rgb)
+    return lst
+
+def generate_colors(col, len):
+    if col == 'hexa':
+        hx_lst = []
+        for i in range(0,len):
+            len = 6
+            alph = 'abcdef'
+            hexa = "".join(random.choices(string.digits+alph, k=len))
+            hexa = '#'+str(hexa)
+            hx_lst.append(hexa)
+        return hx_lst
+    
+    elif col == 'rgb':
+        rgb_lst = []
+        for i in range(0,len):
+            r = randint(0,255)
+            g = randint(0,255)
+            b = randint(0,255)
+            rgb = r,g,b
+            rgb = 'rgb'+str(rgb)
+            rgb_lst.append(rgb)
+        return rgb_lst
+    
+    else:
+        return("Wrong Input")
+            
+def shuffle_list(args):
+    random.shuffle(args)
+    return args
+
+def rand_int():
+    num = 0,1,2,3,4,5,6,7,8,9
+    ran_lst = list(random.sample(num,k=7))
+    return ran_lst
+        
+
+
+    
+
