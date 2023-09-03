ddcd00c3b4ec3560a44ca31ee0f00520aa95e005
Shrey Kshatriya <shreykshatriya@gmail.com>
2023-09-03 02:30:49 -0400

    Day 01

--- Day 01 - Introduction/introduction-to-python.py
+++ Day 01 - Introduction/introduction-to-python.py
@@ -1,83 +1,56 @@
-## Welcome to the Asabeneh's 30 days of python
+# Exercise: Level 1
+# 1. Check the python version you are using
 
-## Introduction
-## Day 1 - Basic Python
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+a,b = 3,4 #defining the operands
 
-print(2+3) ##addition
-print(3-1) ## subtraction
-print(2*3) ##multiplication
-print(3/2) ##division
-print(3 ** 2) ##exponents
-print(19%4) ##modulus or remainder
-print(19//4) ##floor division or quotient
+print("The addition of operands is: ", a+b)    # addition(+)
+print("The subtraction of operands is: ", a-b)    # subtraction(-)
+print("The multiplication of operands is: ", a*b)    # multiplication(*)
+print("The modulus of operands is: ", a%b)    # modulus(%)
+print("The division of operands is: ", a/b)    # division(/)
+print("The exponent of operand 1 to operand 2 is: ", a**b)    # exponential(**)
+print("The floor division of operands is: ", a//b)    # floor division operator(//)
 
-## checking data types using type()
+# 3. Write strings on the python interactive shell. The strings are the following:
 
-a = 'This is the start of 30 days of Python'
-b = 1.00
-c = 100
+print("My name is Shrey")    # Your name
+print("My family name is Kshatriya")    # Your family name
+print("My country is United States of America")    # Your country
+print("I am enjoying 30 days of Python")    # I am enjoying 30 days of python
 
-print(type(a),type(b),type(c)) ## gives the output of string, float, int
+# 4. Check the data types of the following data:
 
-dict = {1:'one', 2: 'two', 3: 'three'}
-list = [1,2,3]
-tuple = (1,2,3)
-set = {1,2,3}
+print("This data type is: ", type(10))    # 10
+print("This data type is: ", type(9.8))    # 9.8
+print("This data type is: ", type(3.14))    # 3.14
+print("This data type is: ", type(4 - 4j))    # 4 - 4j
+print("This data type is: ", type(['Shrey', 'Python', 'United States of America']))    # ['Shrey', 'Python', 'United States of America']
+print("This data type is: ", type("Shrey"))    # Your name
+print("This data type is: ", type("Kshatriya"))    # Your family name
+print("This data type is: ", type("United States of America"))    # Your country
 
-print(type(dict),type(list),type(tuple),type(set)) ## gives the output as dict, list,tuple, set
+# Exercise: Level 2
+# 1. Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, create a python file helloworld.py and repeat questions 1, 2, 3 and 4. Remember to use print() when you are working on a python file. Navigate to the directory where you have saved your file, and run it.
 
+# Exercise: Level 3
+# 1. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
 
-## EXERCISE
+print("Example for integer data type is 10. Type: ", type(10))
+print("Example for float data type is 1.1. Type: ", type(1.1))
+print("Example for complex data type is 3 + 2j. Type: ", type(3 + 2j))
+print("Example for string data type is Hello World. Type: ", type("Hellow World"))
+print("Example for boolean data type is True or False. Type: ", type(True))
+print("Example for list data type is [1,2,3,4,apples]. Type: ", type([1,2,3,4,'apples']))
+print("Example for tuple data type is (1,2,3,4). Type: ", type((1,2,3,4)))
+print('''Example for set data type is {12,3,5,6 }. Type: ''', type({12,3,5,6}))
+print("Example for dictionary data type is {'fruits' : ['apple', 'banana'], 'name' : 'Shrey' }. Type: ", type( {'fruits' : ['apple', 'banana'], 'name' : 'Shrey'}))
 
-## Exercise Level 1 
-    # 1. check the python version
-    ## ans>> to check the python version, go to your command promt/anaconda command promt and type in python --version.
-    ##       this will give you the current version of your python.
+# 2. Find an Euclidian distance between (2, 3) and (10, 8)
 
-    # 2, 3 & 4 are done directly in the commant prompt
+x1,y1 = 2,3
+x2,y2 = 10,8
 
-## Exercise Level 2
-    # Done within the user directory
+e_dist = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
 
-## Exercise Level 3
-    ## 1. Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
-
-print("EXERCISE 3.1 Answers")
-print(type(1)) ## integer
-print(type(10.00)) ##float
-print(type(200 + 10j)) ##complex
-print(type("This is a string")) ##string
-print(type(True)) ##boolean
-print(type([1,2,3])) ##list
-print(type((1,2,3))) ##tuple
-print(type({1,2,3})) ##set
-print(type({1:'a', 2: 'b', 3 : 'c'})) ##dictonary
-
-    ## 2. Find an Euclidian distance between (2, 3) and (10, 8)
-
-## method 1 is to import math and use the sqrt function
-
-print("EXERCISE 3.2 Answers")
-import math
-
-d1 = ((10-2)**2)
-print(d1)
-d2 = ((8-3)**2)
-print(d2)
-d3 = d1 + d2
-print(d3)
-d4 = math.sqrt(d3)
-
-print(f"the distance between (2,3) & (10,8) is {round(d4,2)}")
-
-
-## method 2 is to use string formatting directly with print function
-print('The edist is {0:.2f} and this is method {1:d}'.format(((10-2)**2 + (8-3)**2)**0.5,2))
-
-# method 3 is to use math.dist function directly which calculated the euclidian distance for you
-p1 = 2
-p2 = 3
-q1 = 10
-q2 = 8
-
-print("the edist is {}".format(math.dist([p1,p2],[q1,q2])))
+print("The euclidian distance between (2,3) and (10,8) is: ", e_dist)
